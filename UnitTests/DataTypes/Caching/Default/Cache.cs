7b03b5f2f0ccf16d3c1b1eb0519be5ba60f2bac6
James Craig <admin@gutgames.com>
2013-10-24 16:36:07 -0400

    Finished up basic caching code

---
+++ UnitTests/DataTypes/Caching/Default/Cache.cs
@@ -0,0 +1,114 @@
+﻿/*
+Copyright (c) 2012 <a href="http://www.gutgames.com">James Craig</a>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
+
+using System.Collections.Generic;
+using Utilities.DataTypes;
+using Xunit;
+
+namespace UnitTests.DataTypes.Caching.Default
+{
+    public class Cache
+    {
+        [Fact]
+        public void Create()
+        {
+            Utilities.DataTypes.Caching.Default.Cache Temp = new Utilities.DataTypes.Caching.Default.Cache();
+            Assert.NotNull(Temp);
+            Assert.Equal(0, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(0, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(0, Temp.Values.Count);
+        }
+
+        [Fact]
+        public void Add()
+        {
+            Utilities.DataTypes.Caching.Default.Cache Temp = new Utilities.DataTypes.Caching.Default.Cache();
+            Temp.Add("A", 1);
+            Assert.NotNull(Temp);
+            Assert.Equal(1, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(1, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(1, Temp.Values.Count);
+            Assert.Equal(1, Temp["A"]);
+            Assert.True(Temp.ContainsKey("A"));
+            Assert.True(Temp.Contains(new KeyValuePair<string,object>("A",1)));
+        }
+
+        [Fact]
+        public void Remove()
+        {
+            Utilities.DataTypes.Caching.Default.Cache Temp = new Utilities.DataTypes.Caching.Default.Cache();
+            Temp.Add("A", 1);
+            Assert.NotNull(Temp);
+            Assert.Equal(1, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(1, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(1, Temp.Values.Count);
+            Assert.Equal(1, Temp["A"]);
+            Assert.True(Temp.Remove("A"));
+            Assert.Equal(0, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(0, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(0, Temp.Values.Count);
+            Assert.Equal(null, Temp["A"]);
+        }
+
+        [Fact]
+        public void TryGetValue()
+        {
+            Utilities.DataTypes.Caching.Default.Cache Temp = new Utilities.DataTypes.Caching.Default.Cache();
+            Temp.Add("A", 1);
+            Assert.NotNull(Temp);
+            Assert.Equal(1, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(1, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(1, Temp.Values.Count);
+            object Value=0;
+            Assert.True(Temp.TryGetValue("A", out Value));
+            Assert.Equal(1, Value);
+        }
+
+        [Fact]
+        public void Clear()
+        {
+            Utilities.DataTypes.Caching.Default.Cache Temp = new Utilities.DataTypes.Caching.Default.Cache();
+            Temp.Add("A", 1);
+            Assert.NotNull(Temp);
+            Assert.Equal(1, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(1, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(1, Temp.Values.Count);
+            Temp.Clear();
+            Assert.Equal(0, Temp.Count);
+            Assert.False(Temp.IsReadOnly);
+            Assert.Equal(0, Temp.Keys.Count);
+            Assert.Equal("Default", Temp.Name);
+            Assert.Equal(0, Temp.Values.Count);
+        }
+    }
+}
---
+++ Utilities/DataTypes/Caching/Manager.cs
@@ -0,0 +1,103 @@
+﻿/*
+Copyright (c) 2013 <a href="http://www.gutgames.com">James Craig</a>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
+
+#region Usings
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Utilities.DataTypes.Patterns.BaseClasses;
+using Utilities.IoC.Default;
+using Utilities.IoC.Interfaces;
+using Utilities.DataTypes;
+using Utilities.DataTypes.Caching.Interfaces;
+using Utilities.DataTypes.Caching.Default;
+#endregion
+
+namespace Utilities.DataTypes.Caching
+{
+    /// <summary>
+    /// Caching manager class
+    /// </summary>
+    public class Manager : SafeDisposableBaseClass
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Manager()
+        {
+            Caches = AppDomain.CurrentDomain.GetAssemblies()
+                                            .Types<ICache>()
+                                            .Where(x => !x.Namespace.StartsWith("UTILITIES", StringComparison.OrdinalIgnoreCase))
+                                            .Create<ICache>()
+                                            .ToDictionary(x => x.Name);
+            if (!Caches.ContainsKey("Default"))
+                Caches.Add(AppDomain.CurrentDomain.GetAssemblies()
+                                            .Types<ICache>()
+                                            .Where(x => x.Namespace.StartsWith("UTILITIES", StringComparison.OrdinalIgnoreCase))
+                                            .Create<ICache>()
+                                            .ToDictionary(x => x.Name));
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Caches
+        /// </summary>
+        protected IDictionary<string, ICache> Caches { get; private set; }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Gets the specified cache
+        /// </summary>
+        /// <param name="Name">Name of the cache (defaults to Default)</param>
+        /// <returns>Returns the ICache specified if it exists, otherwise creates a default cache and associates it with the name</returns>
+        public ICache Cache(string Name = "Default")
+        {
+            if (!Caches.ContainsKey(Name))
+                Caches.Add(Name, new Cache());
+            return Caches[Name];
+        }
+
+        /// <summary>
+        /// Disposes of the object
+        /// </summary>
+        /// <param name="Managed">Determines if all objects should be disposed or just managed objects</param>
+        protected override void Dispose(bool Managed)
+        {
+            if (Caches != null)
+            {
+                Caches.ForEach(x => x.Value.Dispose());
+                Caches.Clear();
+            }
+        }
+
+        #endregion
+    }
+}
