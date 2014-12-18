ccea120e0c8a2b49dabc6532973e0d15241294f6
James Craig <admin@gutgames.com>
2014-12-17 22:38:01 -0500

    Added basic Bender tests. Copied over SQL Server code to PostgreSQL schema generator. Will rework/modify.

---
+++ CUL.Bender.Tests/XmlSerializer.cs
@@ -0,0 +1,42 @@
+﻿using CUL.Serialization;
+using System;
+using Xunit;
+
+namespace CUL.Bender.Tests
+{
+    public class XmlSerializerTests
+    {
+        [Fact]
+        public void Creation()
+        {
+            XmlSerializer Serializer = new XmlSerializer();
+            Assert.Equal("text/xml", Serializer.ContentType);
+            Assert.Equal(".xml", Serializer.FileType);
+            Assert.Equal("Bender.XML", Serializer.Name);
+        }
+
+        [Fact]
+        public void Deserialize()
+        {
+            XmlSerializer Serializer = new XmlSerializer();
+            dynamic Object = (TestObject)Serializer.Deserialize(typeof(TestObject), "<?xml version=\"1.0\" encoding=\"utf-8\"?><TestObject><A>5</A><B>ASDF</B></TestObject>");
+            Assert.Equal(5, Object.A);
+            Assert.Equal("ASDF", Object.B);
+        }
+
+        [Fact]
+        public void Serialize()
+        {
+            XmlSerializer Serializer = new XmlSerializer();
+            dynamic Object = new TestObject() { A = 5, B = "ASDF" };
+            Assert.Equal("<?xml version=\"1.0\" encoding=\"utf-8\"?><TestObject><A>5</A><B>ASDF</B></TestObject>", Serializer.Serialize(Object.GetType(), Object));
+        }
+
+        public class TestObject
+        {
+            public int A { get; set; }
+
+            public string B { get; set; }
+        }
+    }
+}
--- CUL.Bender/XmlSerializer.cs
+++ CUL.Bender/XmlSerializer.cs
@@ -19,16 +19,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.*/
 
-#region Usings
-
-using Bender.Configuration;
 using System;
-using System.IO;
-using System.Text;
 using Utilities.DataTypes;
 using Utilities.IO.Serializers.BaseClasses;
-
-#endregion Usings
 
 namespace CUL.Serialization
 {
